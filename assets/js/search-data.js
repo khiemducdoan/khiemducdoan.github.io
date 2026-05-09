deac767d1d73b76e70ea4013533f573ebffa8c36
khiemducdoan <89760611+khiemducdoan@users.noreply.github.com>
2026-05-09 22:49:31 +0000

    Deploying to gh-pages from @ khiemducdoan/khiemducdoan.github.io@abacc3a25e9287ad1e0b9c615a9ec1ec4561ce9b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-curriculum-vitae",
           title: "Curriculum Vitae",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Welcome to my CV !",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
