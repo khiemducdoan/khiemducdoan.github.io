ba4daa4d4fc3fe62f782d8a05e9ef777ed9521d9
khiemducdoan <89760611+khiemducdoan@users.noreply.github.com>
2026-06-06 13:09:30 +0000

    Deploying to gh-pages from @ khiemducdoan/khiemducdoan.github.io@25a9b1564e27df2e4fbf22a86f29cad401656715 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-plotly-js",
+            },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/2018-12-22-distill/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -356,17 +367,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2021/diagrams/";
-          
-        },
-      },{id: "post-telecom-sudparis-moi-et-mon-camarade-hai-anh-le",
-        
-          title: "Telecom SudParis, Moi, et mon camarade Hai-Anh LE",
-        
-        description: "an example of a distill-style blog post and main elements",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/distill/";
           
         },
       },{id: "post-a-post-with-twitter",
